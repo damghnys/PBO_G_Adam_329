4618b6dc15c7cc99d010d9994cce876c913bfe66
unknown <keiixtkjgmail.com>
2025-03-05 10:52:52 +0700

    commit pertama

---
+++ tugas1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class tugas1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Pilih Jenis Login:");
+        System.out.println("1. Admin");
+        System.out.println("2. Mahasiswa");
+        System.out.print("Masukkan pilihan: ");
+        int pilihan = scanner.nextInt();
+        scanner.nextLine();
+
+        if (pilihan == 1) {
+            System.out.print("Masukkan Username: ");
+            String username = scanner.nextLine();
+            System.out.print("Masukkan Password: ");
+            String password = scanner.nextLine();
+
+            if (username.equals("Admin010") && password.equals("password010")) {
+                System.out.println("Login Admin berhasil!");
+            } else {
+                System.out.println("Login gagal! Username atau password salah.");
+            }
+        } else if (pilihan == 2) {
+            System.out.print("Masukkan Nama: ");
+            String nama = scanner.nextLine();
+            System.out.print("Masukkan NIM: ");
+            String nim = scanner.nextLine();
+
+            if (nama.equals("Ken Aryo Bimantoro") && nim.equals("202310370311006")) {
+                System.out.println("Login Mahasiswa berhasil!");
+                System.out.println("Nama: " + nama);
+                System.out.println("NIM: " + nim);
+            } else {
+                System.out.println("Login gagal! Nama atau NIM salah.");
+            }
+        } else {
+            System.out.println("Pilihan tidak valid.");
+        }
+
+        scanner.close();
+    }
+}
